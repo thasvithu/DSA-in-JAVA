54c3f7bd8ac09295fad0b65e1c9b0526f098c158
thasvithu <thasvithu7@gmail.com>
2024-04-01 09:18:22 +0530

    binary search added

--- 04 Searching/Binary Search/Main.java
+++ 04 Searching/Binary Search/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static int binarySearch(int[] arr, int target) {
         int start = 0;
@@ -17,9 +19,28 @@ public class Main {
         return -1;
     }
 
+    public static int binaryRec(int[] arr, int target, int start, int end) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            else if (arr[mid] < target) {
+                return binaryRec(arr, target, mid + 1, end);
+            }
+            else {
+                return binaryRec(arr, target, start, mid - 1);
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 4, 6, 8, 9, 7, 8, 2, 66, 12};
         int target = 66;
+        Arrays.sort(nums);
 
         int result = binarySearch(nums, target);
 
@@ -27,5 +48,8 @@ public class Main {
             System.out.println("Element found at index : " + result);
         else
             System.out.println("Element Not Found");
+
+
+        System.out.println("Search 66 : " + binaryRec(nums, 6, 0, nums.length - 1));
     }
 }
